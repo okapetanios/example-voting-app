f99bfc51bd39fc5735053fedfa05e96355ee3027
Eric Egan <eegan@linuxfoundation.org>
2024-08-06 23:23:24 -0700

    added back version to vote app

--- vote/tests/test_frontend.py
+++ vote/tests/test_frontend.py
@@ -16,10 +16,10 @@ class VoteUnitTest(unittest.TestCase):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
 
-    # def test_version_is_set(self):
-    #     """Assert that version is set"""
-    #     result = self.app.get('/')
-    #     assert 'Version' in str(result.data)
+    def test_version_is_set(self):
+        """Assert that version is set"""
+        result = self.app.get('/')
+        assert 'Version' in str(result.data)
 
     def test_processed_by_is_set(self):
         """Assert that processed by string is set"""
